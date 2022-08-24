f3bf84cf7523db7c94904074e843842a135c640c
Duy Pham <ericpham19@outlook.com>
2022-08-24 15:39:34 +1000

    finished

--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -1,49 +1,31 @@
-import React, {useEffect, useState, useRef} from "react";
+import React, { useEffect, useState, useRef } from "react";
 import GifList from "./GifList";
-import GifSearch from "./GifSearch"
+import GifSearch from "./GifSearch";
 
-function GifListContainer(){
-    const [gifs , setGif] = useState([])
-    const [query , setQuery] = useState("")
-    
-    
-    useEffect(() => {
-       
-        
-            fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=L5UiYwmqhJmCqe7ARupD52qXxWVn6Chq&rating=g&limit=3`)
-            .then((res) => res.json())
-            .then(({data}) => {
-                const gifs = data.map((gif) => ({url: gif.images.original.url}));
-                setGif(gifs)
-            });
+function GifListContainer() {
+  const [gifs, setGif] = useState([]);
+  const [query, setQuery] = useState("");
+  const timer = useRef(null);
+  useEffect(() => {
+    clearTimeout(timer.current);
+    timer.current = setTimeout(() => {
+      fetch(
+        `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=L5UiYwmqhJmCqe7ARupD52qXxWVn6Chq&rating=g&limit=3`
+      )
+        .then((res) => res.json())
+        .then(({ data }) => {
+          const gifs = data.map((gif) => ({ url: gif.images.original.url }));
+          setGif(gifs);
+        });
+    }, 1000);
+  }, [query]);
 
-     
-
-       
-    } , [query])
-   
-    return (
-        <div>
-            <GifList gifs= {gifs} />
-            <GifSearch setQuery= {setQuery} query={query}/>
-            
-            </div>
-    )
-    
+  return (
+    <div>
+      <GifList gifs={gifs} />
+      <GifSearch setQuery={setQuery} query={query} />
+    </div>
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 export default GifListContainer;
